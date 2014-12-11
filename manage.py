3b3418e67d4e1680643e698a264a3dc4d554e101
deshna desai <desai.deshna@gmail.com>
2014-12-11 12:39:02 +0530

    Working barebones application

---
+++ manage.py
@@ -0,0 +1,67 @@
+# Set the path
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from flask.ext.script import Manager, Server
+from snippetlog import app
+from flask import Flask, request, render_template, url_for, redirect
+from flask.views import MethodView
+from wtforms import Form, StringField, validators
+from models import Post, Comment
+manager = Manager(app)
+
+
+# Turn on debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0')
+)
+
+
+class AddPostForm(Form):
+        title= StringField('Title',[validators.Length(min=4,max=25)])
+        subtitle= StringField('Subtitle',[validators.Length(min=0,max=25)])
+        body= StringField('Content',[validators.Length(min=4,max=500)])
+
+class AddCommentForm(Form):
+        comment= StringField('Comment',[validators.Length(min=1,max=100)])
+        author= StringField('Author',[validators.Length(min=1,max=5)])
+        
+@app.route('/')
+def GetList():
+    posts=Post.objects.all()
+    form=AddPostForm(request.form)
+    return render_template("list.html",posts=posts,form=form)
+
+@app.route('/<title>')
+def GetDetail(title):
+        post = Post.objects.get_or_404(title=title)
+        form=AddPostForm(request.form)
+        form2=AddCommentForm(request.form)
+        return render_template("detail.html", post=post, form=form, form2=form2)
+
+@app.route('/addpost',  methods=['GET', 'POST'])
+def addpost():
+        form=AddPostForm(request.form)
+        title = form.title.data
+        subtitle = form.subtitle.data
+        body = form.body.data
+        post=Post(title=title,subtitle=subtitle,body=body)
+        post.save()
+        return redirect(url_for('GetList'))
+
+@app.route('/<title>/addcomment',methods=['GET','POST'])
+def addcomment(title):
+        post = Post.objects.get_or_404(title=title)
+        form=AddCommentForm(request.form)
+        comment= form.comment.data
+        author= form.author.data
+        comment= Comment(author=author, body=comment)
+        post.comments.append(comment)
+        post.save()
+        return redirect(url_for('GetDetail', title=title))
+
+
+if __name__ == "__main__":
+    manager.run()
---
+++ models.py
@@ -0,0 +1,26 @@
+import datetime
+from flask import url_for
+from snippetlog import db
+
+class Post(db.Document):
+        created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+	title = db.StringField(max_length=255, required=True)
+	subtitle = db.StringField(max_length=255, required=True)
+	body = db.StringField(required=True)
+	comments = db.ListField(db.EmbeddedDocumentField('Comment'))
+
+        def get_absolute_url(self):
+                return url_for('post', kwargs={"title": self.title})
+        def __unicode__(self):
+                return self.title
+
+        meta = {
+                'allow_inheritance': True,
+                'indexes': ['-created_at', 'title'],
+                'ordering': ['-created_at']
+            }
+
+class Comment(db.EmbeddedDocument):
+    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
+    body = db.StringField(verbose_name="Comment", required=True)
+    author = db.StringField(verbose_name="Name", max_length=255, required=True)
