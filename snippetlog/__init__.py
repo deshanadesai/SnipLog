6a40e0f65c1c415dbeaf6c3c2bcccb72bd9519c5
deshna desai <desai.deshna@gmail.com>
2014-12-24 21:42:41 +0530

    Adding auth

--- snippetlog/__init__.py
+++ snippetlog/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
-
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {'DB': "my_snippet_log"}
+app.config["MONGODB_SETTINGS"] = {'DB': "my_tumble_log"}
 app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
 db = MongoEngine(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 if __name__ == '__main__':
     app.run()
